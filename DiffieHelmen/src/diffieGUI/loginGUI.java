30d9c9341b0199f5680e063a221bf1603f1a39aa
Hgarnell <hannah@whiterabbit.com>
2022-05-09 10:02:35 +1200

    LoginGUI v1.0
    Created basic login page layout with username and password fields, button and action listener

---
+++ DiffieHelmen/src/diffieGUI/loginGUI.java
@@ -0,0 +1,77 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package diffieGUI;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author hanna
+ */
+public class loginGUI extends JFrame implements ActionListener {
+
+    private JLabel jUsername, jPassword;
+    private JButton jButton1;
+    private JPasswordField jPasswordField1;
+    private JTextField jUserField;
+    private JFrame frame;
+
+    public loginGUI() {
+        super();
+        super.setLayout(null);
+
+        //setting the frame
+        this.setTitle("Diffie Helmen Login");
+        this.setSize(500, 200);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //adding the username login label and text box
+        this.jUsername = new JLabel("Username");
+        this.jUserField = new JTextField();
+        jUsername.setBounds(80, 10, 100, 20);
+        jUserField.setBounds(80, 30, 100, 20);
+        this.add(jUsername);
+        this.add(jUserField);
+        this.setVisible(true);
+
+        //adding the password login label and text box
+        this.jPassword = new JLabel("Password");
+        this.jPasswordField1 = new JPasswordField();
+        jPassword.setBounds(80, 70, 100, 20);
+        jPasswordField1.setBounds(80, 90, 100, 20);
+        this.add(jPassword);
+        this.add(jPasswordField1);
+        this.setVisible(true);
+
+        //adding login button
+        this.jButton1 = new JButton("Login");
+        this.jButton1.setBounds(300, 130, 100, 20);
+        this.jButton1.addActionListener(this);
+        this.add(jButton1);
+        this.setVisible(true);
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String inputUsername = jUserField.getText();
+        String inputPassword = jPassword.getText();
+
+    }
+
+    public static void main(String[] args) {
+        loginGUI login = new loginGUI();
+        //   login.setFrame(this.frame);
+        
+
+    }
+}
