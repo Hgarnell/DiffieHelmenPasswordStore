872b7dae9d7bbfafbb6ef164bcb1bf8ca3888d1f
Hgarnell <hannah@whiterabbit.com>
2022-05-09 10:25:04 +1200

    Error message added to loginGUI
    added Jlabel Jerror to display red text when incorrect pin and password combination is entered.

--- DiffieHelmen/src/diffieGUI/loginGUI.java
+++ DiffieHelmen/src/diffieGUI/loginGUI.java
@@ -4,11 +4,13 @@
  */
 package diffieGUI;
 
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
@@ -19,7 +21,7 @@ import javax.swing.JTextField;
  */
 public class loginGUI extends JFrame implements ActionListener {
 
-    private JLabel jUsername, jPassword;
+    private JLabel jUsername, jPassword, jError;
     private JButton jButton1;
     private JPasswordField jPasswordField1;
     private JTextField jUserField;
@@ -59,6 +61,14 @@ public class loginGUI extends JFrame implements ActionListener {
         this.add(jButton1);
         this.setVisible(true);
 
+        //formating the errormessage
+        this.jError = new JLabel(" oh a no!");
+        jError.setForeground(Color.red);
+        this.jError.setBounds(300, 40, 100, 20);
+        jError.setVisible(false);
+        this.add(jError);
+        //this.setVisible(true);
+
     }
 
     @Override
@@ -66,12 +76,22 @@ public class loginGUI extends JFrame implements ActionListener {
         String inputUsername = jUserField.getText();
         String inputPassword = jPassword.getText();
 
+        //test parameters
+        if (inputUsername.equals("test") && inputPassword.equals("test")) {
+            JOptionPane.showMessageDialog(null, "Login Successful");
+
+        } else {
+            //JOptionPane.showMessageDialog(null, "Login bad");
+            this.jError.setVisible(true);
+            this.setVisible(true);
+
+        }
+
     }
 
     public static void main(String[] args) {
         loginGUI login = new loginGUI();
         //   login.setFrame(this.frame);
-        
 
     }
 }
